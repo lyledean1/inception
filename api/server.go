cfbe8e5f0d7599fab8c239d129de8334c0f1b5d2
Lyle Dean <lylebd1@gmail.com>
2020-04-11 20:17:51 +0100

    add k8s

--- api/server.go
+++ api/server.go
@@ -1,10 +1,11 @@
 package api
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/lyledean1/inception/image"
 )
-
 
 func SetupRouter(imageService image.Service) *gin.Engine {
 	gin.SetMode(gin.DebugMode)
@@ -12,12 +13,13 @@ func SetupRouter(imageService image.Service) *gin.Engine {
 	r.RedirectTrailingSlash = false
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
-	imageCtrl := NewImageController(imageService)
-	image := r.Group("/api/v1/image")
-	image.POST("", imageCtrl.PredictLabelFromImage)
 	r.GET("/", func(c *gin.Context) {
 		//health check
 		c.JSON(200, nil)
 	})
+	imageCtrl := NewImageController(imageService)
+	image := r.Group("/api/v1/image", gin.BasicAuth(gin.Accounts{
+		"lyle": os.Getenv("INCEPTION_PASSWORD")}))
+	image.POST("", imageCtrl.PredictLabelFromImage)
 	return r
 }

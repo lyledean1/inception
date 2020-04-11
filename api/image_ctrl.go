0e7793bce4b20b1f7c9ac835289aded54e2d42f4
Lyle Dean <lylebd1@gmail.com>
2020-04-11 20:22:11 +0100

    add k8s

--- api/image_ctrl.go
+++ api/image_ctrl.go
@@ -2,10 +2,11 @@ package api
 
 import (
 	"bytes"
-	"github.com/gin-gonic/gin"
-	"github.com/lyledean1/inception/image"
 	"io"
 	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lyledean1/inception/image"
 )
 
 type ImageController struct {
@@ -21,7 +22,7 @@ func NewImageController(service image.Service) *ImageController {
 func (ctrl *ImageController) PredictLabelFromImage(c *gin.Context) {
 	imageFile, header, err := c.Request.FormFile("image")
 	if err != nil {
-		c.JSON(400, nil )
+		c.JSON(400, nil)
 		return
 	}
 	imageName := strings.Split(header.Filename, ".")[:1][0]
@@ -29,12 +30,12 @@ func (ctrl *ImageController) PredictLabelFromImage(c *gin.Context) {
 	var imageBuffer bytes.Buffer
 	_, err = io.Copy(&imageBuffer, imageFile)
 	if err != nil {
-		c.JSON(400, nil )
+		c.JSON(400, nil)
 		return
 	}
 	cmd, err := ctrl.service.ProcessImageWithLabelPredictions(imageName, imageBuffer)
 	if err != nil {
-		c.JSON(400, nil )
+		c.JSON(400, nil)
 		return
 	}
 	c.JSON(200, cmd)
